47ca0008618ea1735237e980844e0775ef4a0993
kirstenbryan <kirstebs@miamioh.edu>
2017-03-13 00:50:14 -0400

    Update P3_26.java

--- Java/Big Java Late Objects (Horstman)/Chapter 3/P3_26.java
+++ Java/Big Java Late Objects (Horstman)/Chapter 3/P3_26.java
@@ -14,16 +14,16 @@ import java.util.Scanner;
  *		a. Only numbers up to 3,999 are represented.
  *		b. As in the decimal system, the thousands, hundreds, tens, and ones are expressed separately.
  *		c. The  numbers 1 to 9 are express as:
- *  		I  		1 
+ *  		I  	1 
  *  		II  	2 
  *  		III  	3 
  *  		IV  	4 
- *  		V  		5 
+ *  		V 	5 
  *  		VI  	6 
  *  		VII  	7		 
  *  		VIII  	8 
  *  		IX  	9 
- *  As you can see, an I preceding a V or X is subtracted from the value, and you can never have more than three I’s in 
+ *  As you can see, an I preceding a V or X is subtracted from the value, and you can never have more than three Iâ€™s in 
  *  a row. 
  *  	d. Tens and hundreds are done the same way, except that the letters X, L, C and C, D, M are used instead of I,
  *  	   V, X, respectively. 
