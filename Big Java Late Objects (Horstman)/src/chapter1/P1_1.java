9da331655b58ed8f446b8ebe299d4aa18f734e3c
Bryan <Bryan@Stormzyy>
2021-12-12 10:01:08 -0600

    BK 12/12 - Test

---
+++ Big Java Late Objects (Horstman)/src/chapter1/P1_1.java
@@ -0,0 +1,19 @@
+package chapter1;
+
+/** 
+ * 
+ * Write a program that prints a greeting of your choice, perhaps in a language other than English.
+ * 
+ * @author Bryan S. Kirsten
+ *
+ */
+
+public class P1_1 {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Bonjour, je m'appelle Bryan Kirsten. Comment ca va?");
+		
+	}
+
+}
